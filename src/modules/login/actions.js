50bc0be34074ee5a29dabdaa55b60181dc79a360
Rangga Rifqi Pratama <rangga.rifqi.pratama@gmail.com>
2018-07-23 22:17:01 +0700

    Get Role Data

--- src/modules/login/actions.js
+++ src/modules/login/actions.js
@@ -5,10 +5,21 @@ export default {
   loginAction: async (context, {email, password}) => {
     try {
       let { data } = await Vue.axios.post(`/users/login`, { email, password });
-      console.log(data);
+      let { data: userData } = await Vue.axios.get(`/users/${data.userId}`, {
+        params: {
+          access_token: data.id,
+          filter: {
+            include: 'role'
+          }
+        }
+      });
+
+      const role = userData.role[0];
+
       localStorage.setItem('access_token', data.id);
       localStorage.setItem('user_id', data.userId);
       localStorage.setItem('ttl', data.ttl);
+      localStorage.setItem('role', role.name);
       context.commit('reset');
       router.push('/');
     } catch (e) {
@@ -20,23 +31,5 @@ export default {
         type: 'error'
       }, { root: true });
     }
-
-
-    //Well, this is just a simulation...so...deal with it *Put on black glasses*
-    // setTimeout(() => {
-    //   if (email == 'admin@admin.com' && password == 'admin') {
-    //     localStorage.setItem('access_token', 'lololololololo');
-    //     localStorage.setItem('user_id', 1);
-    //     context.commit('reset');
-    //     router.push('/');
-    //   } else {
-    //     context.commit('reset');
-    //     context.commit('notify/showNotify', {
-    //       title: 'Login Failed',
-    //       text: 'Wrong Email or Password',
-    //       type: 'error'
-    //     }, { root: true });
-    //   }
-    // }, 3000);
   }
 }
