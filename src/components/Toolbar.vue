a5d61366e7492da3c48480f93cd95a4a30739b63
Rangga Rifqi Pratama <rangga.rifqi.pratama@gmail.com>
2018-07-24 23:53:12 +0700

    Ganti Title

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -8,7 +8,7 @@
   >
     <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
       <v-toolbar-side-icon @click.stop="toggleDrawer(!drawer)"></v-toolbar-side-icon>
-      <span class="hidden-sm-and-down">Ranggo CMS</span>
+      <span class="hidden-sm-and-down">Employee Journal</span>
     </v-toolbar-title>
     <v-text-field
       flat
